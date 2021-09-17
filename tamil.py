020a59fe1673766982d4b1eb17d116455df9a656
Ashwin Ram <ashwin.ram0308@gmail.com>
2021-09-17 17:15:37 -0500

    Create tamil.py

---
+++ tamil.py
@@ -0,0 +1,13 @@
+from inltk.inltk import setup
+from inltk.inltk import predict_next_words
+from inltk.inltk import get_similar_sentences
+setup('ta')
+example_sent = "உங்களைப் பார்த்து நிறைய நாட்கள் ஆகிவிட்டது"
+# Predict next 'n' tokens
+n = 5
+pred_sent = predict_next_words(example_sent, n, 'ta')
+# Get 'n' similar sentence
+n = 2
+simi_sent = get_similar_sentences(example_sent, n, 'ta')
+print("Predicted Words:", pred_sent)
+print("Similar Sentences:", simi_sent)
